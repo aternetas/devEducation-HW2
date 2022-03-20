743b8a7230cc4d83e6034edc2975c54a4d484f6b
lena falina <aternetas@gmail.com>
2022-03-21 01:15:14 +0300

    ArrayList (tasks 11-18)

--- ArrayList/Program.cs
+++ ArrayList/Program.cs
@@ -1,11 +1,11 @@
 ﻿using MyLists;
 
-int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 ArrayList list = new ArrayList(array);
 //list.RemoveRangeFromIndex(3, 5);
 //list.Write();
-int result = list.GetLenght();
-Console.Write(result);
+list.Reverse();
+list.Write();
 
 
 
--- MyArrayList/ArrayList.cs
+++ MyArrayList/ArrayList.cs
@@ -1,4 +1,6 @@
-﻿namespace MyLists
+﻿using System;
+
+namespace MyLists
 {
     public class ArrayList
     {
@@ -25,6 +27,7 @@
             _array = array;
             Lenght = _array.Length;
         }
+
         // добавление значения в конец (task 1)
         public void AddLast(int value)
         {
@@ -35,6 +38,7 @@
             _array[Lenght] = value;
             Lenght++;
         }
+
         // добавление значения в начало (task 2)
         public void AddFirst(int value)
         {
@@ -45,6 +49,7 @@
             _array[0] = value;
             Lenght++;
         }
+
         // добавление значения по индексу (task 3)
         public void Insert(int index, int value)
         {
@@ -56,17 +61,20 @@
             _array[index] = value;
             Lenght++;
         }
+
         // удаление из конца одного элемента (task 4)
         public void RemoveLast()
         {
             Lenght--;
         }
+
         // удаление из начала одного элемента (task 5)
         public void RemoveFirst()
         {
             MoveOneStepLeft(0);
             Lenght--;
         }
+
         // удаление по индексу одного элемента (task 6)
         public void RemoveByIndex(int index)
         {
@@ -80,6 +88,7 @@
             }
             Lenght--;
         }
+
         // удаление из конца N элементов (task 7)
         public void RemoveRangeFromEnd(int count)
         {
@@ -91,6 +100,7 @@
             }
             _array = newArray;
         }
+
         // удаление из начала N элементов (task 8)
         public void RemoveRangeFromBeginning(int count)
         {
@@ -102,6 +112,7 @@
             }
             _array = newArray;
         }
+
         // удаление по индексу N элементов (task 9)
         public void RemoveRangeFromIndex(int count, int index)
         {
@@ -117,19 +128,139 @@
             }
             _array = newArray;
         }
+
         // вернуть длину (task 10)
         public int GetLenght()
         {
             return Lenght;
         } 
+
         // доступ по индексу (task 11)
+        public int GetValue(int index)
+        {
+            if (Lenght == 0)
+            {
+                throw new Exception("ArrayList is empty");
+            }
+            return _array[index];
+        }
+
         // первый индекс по значению(-1 - не нашел) (task 12)
+        public int ReturnFirstIndex(int value)
+        {
+            int firstIndex = -1;
+            for (int i = 0; i < Lenght; i++)
+            {
+                if (_array[i] == value)
+                {
+                    firstIndex = i;
+                    break;
+                }
+            }
+            return firstIndex;
+        }
+
         // изменение по индексу (task 13)
+        public void ChangeByIndex(int index, int value)
+        {
+            if (Lenght == 0)
+            {
+                throw new Exception("ArrayList is empty");
+            }
+            if (index > Lenght)
+            {
+                throw new Exception("The index does not exist");
+            }
+            _array[index] = value;
+        }
+
         // реверс(123 -> 321) (task 14)
+        public void Reverse()
+        {
+            int newLenght = _array.Length;
+            int[] newArray = new int[newLenght];
+            Copy(newArray);
+            int n = newLenght / 2;
+            for (int i = 0; i < n; i++)
+            {
+                int tmp = _array[i];
+                _array[i] = newArray[newLenght - (i + 1)];
+                newArray[newLenght - (i + 1)] = tmp;
+            }
+            _array = newArray;
+        }
+
         // поиск значения максимального элемента (task 15)
+        public int FindMaxValue()
+        {
+            if (Lenght == 0)
+            {
+                throw new Exception("ArrayList is empty");
+            }
+            int maxValue = _array[0];
+            for (int i = 0; i < Lenght; i++)
+            {
+                if (_array[i] > maxValue)
+                {
+                    maxValue = _array[i];
+                }
+            }
+            return maxValue;
+        }
+
         // поиск значения минимального элемента (task 16)
+        public int FindMinValue()
+        {
+            if (Lenght == 0)
+            {
+                throw new Exception("ArrayList is empty");
+            }
+            int minValue = _array[0];
+            for (int i = 0; i < Lenght; i++)
+            {
+                if (_array[i] > minValue)
+                {
+                    minValue = _array[i];
+                }
+            }
+            return minValue;
+        }
         // поиск индекс максимального элемента (task 17)
+        public int FindIndexOfMaxValue()
+        {
+            if (Lenght == 0)
+            {
+                throw new Exception("ArrayList is empty");
+            }
+            int index = 0;
+            for (int i = 0; i < Lenght; i++)
+            {
+                if (_array[i] > _array[index])
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
         // поиск индекс минимального элемента (task 18)
+        public int FindIndexOfMinValue()
+        {
+            if (Lenght == 0)
+            {
+                throw new Exception("ArrayList is empty");
+            }
+            int index = 0;
+            for (int i = 0; i < Lenght; i++)
+            {
+                if (_array[i] < _array[index])
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
         // сортировка по возрастанию (task 19)
         // сортировка по убыванию (task 20)
         // удаление по значению первого(вернуть индекс)(-1 - ничего не удалил) (task 21)
